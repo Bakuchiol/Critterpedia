4b967e44c3c4e6f3d7e3b8520db7488fc4d3a575
Lynette <lynettecargo@Lynettes-Air.home>
2023-08-09 23:51:19 -0500

    added images

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ slider.addEventListener('mousedown', (e) => {
     slider.classList.add('active')
     // console.log(e.pageX);
     startX = e.pageX - slider.offsetLeft;
-    console.log(startX);
+    scrollLeft = slider.scrollLeft
+    // console.log(startX);
 });
 
 slider.addEventListener('mouseleave', () => {
@@ -23,12 +24,18 @@ slider.addEventListener('mouseup', () => {
 
 });
 
-slider.addEventListener('mousemove', () => {
+slider.addEventListener('mousemove', (e) => {
     console.log(isDown);
     console.log('WORKS');
     // stops the function from running
     if(!isDown) return
-    console.log(startX);
+    // console.log(startX);
+    e.preventDefault() // stop selecting of text inside/weird clicks
+    // where cursor is when move left/right
+    const x = e.pageX - slider.offsetLeft;
+    // console.log({x, startX});
+    const walk = (x -startX) * 3;
+    slider.scrollLeft = scrollLeft - walk;
 });
 
 
